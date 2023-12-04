c9e7b7bd81a6855c664dda80b8361fea0ffb7597
Thiago Oliveira Azevedo de Carvalho <69779692+Th-Oliveira@users.noreply.github.com>
2023-12-04 14:02:35 -0300

    Commit 6

--- script.js
+++ script.js
@@ -10,7 +10,6 @@ const img = document.querySelectorAll('.title-flex-perguntas-imagens img')
 const divOfImg = document.querySelectorAll('.title-flex-perguntas-imagens')
 const p = document.querySelectorAll('.faq p')
 
-console.log(p)
 function accordList() {
   if(this.parentElement.parentElement.children[1].classList.contains('p-ativo')) {
     this.parentElement.parentElement.children[1].classList.remove('p-ativo')
